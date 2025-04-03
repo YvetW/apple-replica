d7263caeaed4b9d7c477586d02fecfd88fca58ab
Yvette <Yvettewang@outlook.com>
2025-04-03 23:38:37 +0200

    feat(iphonepage): add new structure

--- src/pages/IphonePage.tsx
+++ src/pages/IphonePage.tsx
@@ -1,6 +1,26 @@
+import '../styles/pages/iphonePage.scss';
+
 export function IphonePage() {
     return (
-        <h1>这是IphonePage</h1>
+        <div id="iphone-page">
+            <nav id="chapternav">
+                <div className="chapternav-wrapper">
+                    <ul>
+                        <li className="chapternav-item chapternav-item-iphone-16-pro">
+                            <a href="#">
+                                <figure className="chapternav-icon"></figure>
+                                <span className="chapternav-label"></span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="chapternav-paddles">
+                        <button className="chapternav-paddle chapternav-paddle-left"></button>
+                        <button className="chapternav-paddle chapternav-paddle-right"></button>
+                    </div>
+                </div>
+            </nav>
+            <main id="main"></main>
+        </div>
 
     );
 }
